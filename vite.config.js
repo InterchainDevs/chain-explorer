3eab7d8733456639f9084b6c4d74b5715599dd42
atmoner <contact.atmoner@gmail.com>
2024-08-07 13:19:12 +0200

    update NODE_ENV

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/chain-explorer/",
+  base: process.env.NODE_ENV === "production" ? "/chain-explorer/" : "/",
   plugins: [
     vue(),
   ],
