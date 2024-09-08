2a7c6d7f4923af0091cd86c3ada6d47d37d61df8
atmoner <contact.atmoner@gmail.com>
2024-09-08 18:30:49 +0200

    Improve block detail view

--- src/views/BlockDetailsView.vue
+++ src/views/BlockDetailsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row no-gutters class="mt-7">
+  <v-row no-gutters >
     <v-col cols="12" md="3" lg="4">
       <v-sheet border rounded="lg" class="mb-6 mr-0 mr-md-3 pa-2">
         Block Height
@@ -42,11 +42,20 @@
           <tbody>
             <tr>
               <td>Block time</td>
-              <td>{{ blockInfo.block?.header.time }}</td>
+              <td>              
+              {{               
+                moment(blockInfo.block?.header.time).format(
+                  "MMMM Do YYYY, h:mm:ss a",
+                )
+              }}</td>
             </tr>
             <tr>
               <td>ChainId</td>
-              <td>{{ blockInfo.block?.header.chain_id }}</td>
+              <td>
+                <v-chip label>
+                  {{ blockInfo.block?.header.chain_id }}
+                </v-chip>
+              </td>
             </tr>
             <tr>
               <td>Proposer Address</td>
@@ -69,8 +78,11 @@
           </thead>
           <tbody>
             <tr v-for="item in allTransactions" :key="item.name">
-              <td>{{ item.type }}</td>
-              <td>{{ item.calories }}</td>
+              <td>
+                <v-chip class="ma-2" label :color="item.formatMsg.color">
+                  {{ item.formatMsg.typeReadable }} 
+                </v-chip> </td>
+              <td><!--  {{ item.formatMsg }} --></td>
             </tr>
           </tbody>
         </v-table>
@@ -87,10 +99,14 @@ import { Tendermint37Client } from "@cosmjs/tendermint-rpc";
 import { decodeTxRaw } from "@cosmjs/proto-signing";
 import { fromBase64, fromUtf8 } from "@cosmjs/encoding";
 import axios from "axios";
+import moment from "moment";
+
+import { setMsg } from "@/libs/msgType";
 
 export default {
   name: "BlocksView",
   data: () => ({
+    moment,
     blocks: [],
     isloaded: false,
     lastblock: [],
@@ -126,10 +142,22 @@ export default {
         );
         if (foundMsgType) {
           const msg = foundMsgType[1].decode(decodedTx.body.messages[j].value);
-          console.log(msg);
+          console.log(decodedTx);
+
+          let formatMsg = setMsg(
+            decodedTx.body.messages[j].typeUrl,
+            "",
+            "",
+            "",
+            "",
+          );
+
+
+
           this.allTransactions.push({
             type: decodedTx.body.messages[j].typeUrl,
             message: msg,
+            formatMsg: formatMsg,
           });
         } else {
           console.log(
